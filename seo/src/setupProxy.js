64b3f518c9b73ed28b4efb96f69c3f8445ac819f
Javon Heng <javonheng@gmail.com>
2019-12-06 10:39:27 +0800

    setupproxy

--- seo/src/setupProxy.js
+++ seo/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     // add other server routes to path array
-    app.use(proxy(['/createeventapi', '/updatenewsapi', '/addmembersapi', '/addagendasapi', '/rentalstatusapi', '/attendeesapi', '/savedpresetapi', '/userslogin'], { target: 'http://127.0.0.1:4000', secure: false, changeOrigin: true }));
-    app.use(proxy(['/upload', '/files', '/files/:filename', '/image/:filename', '/files/:id' ], { target: 'http://127.0.0.1:5005', secure: false, changeOrigin: true }));
+    app.use(proxy(['/createeventapi', '/updatenewsapi', '/addmembersapi', '/addagendasapi', '/rentalstatusapi', '/attendeesapi', '/savedpresetapi', '/userslogin'], { target: 'http://localhost:4000' }));
+    app.use(proxy(['/upload', '/files', '/files/:filename', '/image/:filename', '/files/:id' ], { target: 'http://localhost:5005' }));
 }
